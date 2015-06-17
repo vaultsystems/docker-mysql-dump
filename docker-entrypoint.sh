1315b3aaabe1d81a3ddac1acc005a98eac28b492
Christoph Dwertmann <cdwertmann@gmail.com>
2015-06-17 12:19:05 +1000

    add mysqldump options

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -17,7 +17,7 @@ cp /usr/share/zoneinfo/Australia/Sydney /etc/localtime
 cd /mnt
 while true; do
   f=`date '+%Y-%m-%d_%H-%M-%S'`.sql.gz
-  mysqldump -h mysql_backup --all-databases | gzip > $f
+  mysqldump -h mysql_backup  --max_allowed_packet=256M -R --opt --skip-lock-tables --all-databases | gzip > $f
   swift upload mysql-dump $f
   rm $f
   sleep 1800
